14912a4c00f6dbf390267ad270758273e00c4ab7
Amit Raj <amitraj4381@gmail.com>
2025-05-15 23:49:30 +0530

    added image optimization

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import viteImagemin from 'vite-plugin-imagemin';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-})
+  plugins: [
+    react(),
+    svgr(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 70,
+      },
+      pngquant: {
+        quality: [0.65, 0.8],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+      webp: {
+        quality: 75,
+      },
+    }),
+  ],
+});
